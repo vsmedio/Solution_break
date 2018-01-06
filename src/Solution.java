e50a01736c2cb074e3e9581a6c741aae358edd78
Arvis Abromovics <info@vsmedio.lv>
2018-01-06 14:10:24 +0100

    Update Solution.java
    komentāru labojumi

--- src/Solution.java
+++ src/Solution.java
@@ -4,10 +4,10 @@ public class Solution {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
             if (i == 4) {
-                break; // завершить цикл, если i = 4
+                break; // iziet no cikla, ja i = 4
             }
             System.out.println("i: " + i);
         }
-        System.out.println("Цикл завершен.");
+        System.out.println("Cikls pabeigts!");
     }
 }
